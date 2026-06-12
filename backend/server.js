58cb5c6493ea9173c6fb3a8a8e2e7418a50b7cf3
vishalt548 <vtipule@gmail.com>
2026-06-12 16:28:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const User = require('./models/User');
-
+const authRoutes = require('./routes/auth');
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -14,6 +14,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('DealDhamaka DB Connected 🔥'))
 .catch(err => console.log(err));
+app.use('/api/auth', authRoutes);
 
 // Register
 app.post('/api/register', async (req, res) => {
